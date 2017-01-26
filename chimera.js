5d1da9b8df2f0ba6ea1325741773e20de57519f3
Angus Goldsmith <angus@dashery.co.uk>
2017-01-26 17:42:17 +0000

    Changed to provide a function to be called rather than executing automatically.

--- chimera.js
+++ chimera.js
@@ -1,17 +1,17 @@
-function insertAfter(referenceNode, newNode) {
-  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-
-document.addEventListener("DOMContentLoaded", function (event) {
+function chimera() {
   const button = document.createElement('button')
   button.innerHTML = 'Annotation';
   button.addEventListener('click', fetchAnnotation)
 
   insertAfter(document.querySelector('.js-notifications'), button)
-});
+}
 
 function fetchAnnotation() {
   fetch('/asset-bank/go/reverse-proxy/annotations/test', { credentials: 'include' })
     .then(response => response.json())
     .then(annotation => alert(JSON.stringify(annotation)));
+}
+
+function insertAfter(referenceNode, newNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
