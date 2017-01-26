4dedf2da771e25ea7ee3c163aac3e39208729f9e
Angus Goldsmith <angus@dashery.co.uk>
2017-01-26 17:28:14 +0000

    Initial commit.

---
+++ chimera.js
@@ -0,0 +1,15 @@
+const button = document.createElement('button')
+button.innerHTML = 'Annotation';
+button.addEventListener('click', fetchAnnotation)
+
+insertAfter(document.querySelector('.js-notifications'), button)
+
+function fetchAnnotation() {
+  fetch('/asset-bank/go/reverse-proxy/annotations/test', { credentials: 'include' })
+    .then(response => response.json())
+    .then(annotation => alert(JSON.stringify(annotation)));
+}
+
+function insertAfter(referenceNode, newNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
